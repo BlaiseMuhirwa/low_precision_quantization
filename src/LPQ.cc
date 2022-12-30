625a3095d326474a03b7095f0d9eda3b156db215
Blaise Muhirwa <blaise@thirdai.com>
2022-12-29 23:48:40 -0600

    edit CMakeLists.txt

--- src/LPQ.cc
+++ src/LPQ.cc
@@ -1,33 +1,35 @@
 #include "LPQ.h"
 #include <cmath>
+#include <iostream>
 #include <math.h>
 #include <numeric>
 // #include <omp.h>
 
 namespace lpq {
 
+template <typename PRECISION_TYPE>
+LowPrecisionQuantizer<PRECISION_TYPE>::LowPrecisionQuantizer()
+    : _bit_width(8 * sizeof(PRECISION_TYPE)) {}
 
 template <typename PRECISION_TYPE>
-LowPrecisionQuantizer<PRECISION_TYPE>::LowPrecisionQuantizer(uint32_t bit_width)
-    : _bit_width(bit_width) {}
-
-template <typename PRECISION_TYPE>
-std::vector<std::vector<PRECISION_TYPE>> LowPrecisionQuantizer<PRECISION_TYPE>::quantizeVectors(
+std::vector<std::vector<PRECISION_TYPE>>
+LowPrecisionQuantizer<PRECISION_TYPE>::quantizeVectors(
     const std::vector<std::vector<float>> &vectors) {
 
   if (vectors.size() == 0) {
     return {};
   }
-  uint32_t vector_size = vectors.size();
-  std::vector<std::vector<PRECISION_TYPE>> quantized_vectors(vector_size);
+  std::vector<std::vector<PRECISION_TYPE>> quantized_vectors;
 
-#pragma omp parallel default none shared(vectors, quantized_vectors)
+#pragma omp parallel for default(none) shared(vectors, quantized_vectors)
   for (const auto &vector : vectors) {
-#pragma omp critical
-    auto [mean, variance] = computeVectorWiseStatistics(vector);
-    std::vector<PRECISION_TYPE> quantized_vector(vector.size());
-    for (const auto &value : vector) {
-      quantized_vector.push_back(quantize(value, mean, variance));
+    auto [mean, stddev] = computeVectorWiseStatistics(vector);
+    std::vector<PRECISION_TYPE> quantized_vector;
+
+    for (uint32_t vec_index = 0; vec_index < vector.size(); vec_index++) {
+      PRECISION_TYPE quantized_value =
+          quantize(vector[vec_index], mean, stddev);
+      quantized_vector.push_back(quantized_value);
     }
     quantized_vectors.emplace_back(std::move(quantized_vector));
   }
@@ -43,28 +45,42 @@ lpq::LowPrecisionQuantizer<PRECISION_TYPE>::computeVectorWiseStatistics(
   }
   auto const count = static_cast<float>(vector.size());
 
-  auto mean = std::accumulate(vector.begin(), vector.end(), 0) / count;
-  auto variance = 0;
-  std::for_each(vector.begin(), vector.end(), [&variance, &mean](float item)
-  {
+  float mean = std::accumulate(vector.begin(), vector.end(), 0.000000) / count;
+  float variance = 0.000000;
+  std::for_each(vector.begin(), vector.end(), [&variance, &mean](float item) {
     variance += (item - mean) * (item - mean);
   });
-  variance = sqrt(variance);
-  return std::make_tuple(mean, variance);
+  variance /= (vector.size() - 1);
+  float stddev = sqrt(variance);
+  return std::make_tuple(mean, stddev);
 }
 
 template <typename PRECISION_TYPE>
-PRECISION_TYPE lpq::LowPrecisionQuantizer<PRECISION_TYPE>::quantize(float value, float mean,
-                                                float standard_deviation) {
+PRECISION_TYPE
+lpq::LowPrecisionQuantizer<PRECISION_TYPE>::quantize(float value, float mean,
+                                                     float standard_deviation) {
   auto sb = mean - standard_deviation;
   auto se = mean + standard_deviation;
 
-  if (value < mean) {
-    return -pow(2, _bit_width - 1);
-  } else if (value > mean) {
-    return pow(2, _bit_width - 1);
+  if (value < sb) {
+    auto val = -((1 << (_bit_width - 1)) - 1);
+    return val;
+  } else if (value > se) {
+    auto val = (1 << (_bit_width - 1)) - 1;
+    return val;
   }
-  return floor(pow(2, _bit_width) * ((value - mean) / (se - sb)));
+
+  auto val =
+      floor((1 << (_bit_width - 1)) * ((value - mean) / standard_deviation));
+  PRECISION_TYPE largest_int = (1 << (_bit_width - 1)) - 1;
+  PRECISION_TYPE smallest_int = -(largest_int + 1);
+
+  if (val > largest_int) {
+    return largest_int;
+  } else if (val < smallest_int) {
+    return smallest_int;
+  }
+  return val;
 }
 
 // Template specialization for int8 and int16 types
--- src/LPQ.h
+++ src/LPQ.h
@@ -4,21 +4,25 @@
 #include <string>
 #include <tuple>
 #include <vector>
-#include <cereal/access.hpp>
+// #include <cereal/archives/binary.hpp>
 
 namespace lpq {
-template <typename PRECISION_TYPE> 
+
+template <typename PRECISION_TYPE>
+
 class LowPrecisionQuantizer {
-  public:
-    explicit LowPrecisionQuantizer(uint32_t bit_width);
+public:
+  LowPrecisionQuantizer();
 
-    std::vector<std::vector<PRECISION_TYPE>>
-    quantizeVectors(const std::vector<std::vector<float>> &vectors);
+  std::vector<std::vector<PRECISION_TYPE>>
+  quantizeVectors(const std::vector<std::vector<float>> &vectors);
 
-  private:
-    std::tuple<float, float>
-    computeVectorWiseStatistics(const std::vector<float> &vector);
-    PRECISION_TYPE quantize(float value, float mean, float standard_deviation);
-    uint32_t _bit_width;
+  constexpr uint32_t getBitWidth() const { return _bit_width; }
+
+private:
+  std::tuple<float, float>
+  computeVectorWiseStatistics(const std::vector<float> &vector);
+  PRECISION_TYPE quantize(float value, float mean, float standard_deviation);
+  uint32_t _bit_width;
 };
 } // namespace lpq
